3175b6e1af45d7fce9b70db98833aa02ec729d46
WuliYongShun <471972509@qq.com>
2020-08-11 07:31:57 +0800

    添加中断实验程序

---
+++ 14_imx6uirq/.imx6uirq.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/mnt/hgfs/EmbeddedDevelopment/linux/driver/14_imx6uirq/imx6uirq.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /mnt/hgfs/EmbeddedDevelopment/linux/driver/14_imx6uirq/imx6uirq.ko /mnt/hgfs/EmbeddedDevelopment/linux/driver/14_imx6uirq/imx6uirq.o /mnt/hgfs/EmbeddedDevelopment/linux/driver/14_imx6uirq/imx6uirq.mod.o
---
+++ 14_imx6uirq/.tmp_versions/imx6uirq.mod
@@ -0,0 +1,2 @@
+/mnt/hgfs/EmbeddedDevelopment/linux/driver/14_imx6uirq/imx6uirq.ko
+/mnt/hgfs/EmbeddedDevelopment/linux/driver/14_imx6uirq/imx6uirq.o
